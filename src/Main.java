6c418f1863c53da943a28b1e5a1bcdb2405432b2
Stella-Brumatti <stella.wma@gmail.com>
2022-07-24 02:47:26 -0300

    versão final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -11,18 +10,50 @@ public class Main {
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso1.setTitulo("curso js");
-        curso1.setDescricao("descrição curso js");
-        curso1.setCargaHoraria(4);
+        curso2.setTitulo("curso js");
+        curso2.setDescricao("descrição curso js");
+        curso2.setCargaHoraria(4);
+
 
         Mentoria mentoria = new Mentoria();
         mentoria.setTitulo("mentoria de java");
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devCamila = new Dev();
+        devCamila.setNome("Camila");
+        devCamila.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Camila" +devCamila.getConteudoInscritos());
+
+        devCamila.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Camila" +devCamila.getConteudoInscritos());
+        System.out.println("Conteúdos Concluidos Camila" +devCamila.getConteudosConcluidos());
+        System.out.println("XP:" + devCamila.calcularTotalXp());
+
+        System.out.println("------");
+
+        Dev devStella = new Dev();
+        devStella.setNome("Stella");
+        devStella.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Stella" +devStella.getConteudoInscritos());
+        devStella.progredir();
+        System.out.println("-");
+        System.out.println("Conteúdos Inscritos Stella" +devStella.getConteudoInscritos());
+        System.out.println("Conteúdos Concluidos Stella" +devStella.getConteudosConcluidos());
+        System.out.println("XP:" + devStella.calcularTotalXp());
+
     }
 
 }
